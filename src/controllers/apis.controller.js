d642c6f518b2ad35b80a509fdb186d863192a842
Edgar Castillo Ramírez <A00827826@itesm.mx>
2022-05-23 19:30:08 -0500

    Changes on Queries
    DB changed, Queries modified

--- src/controllers/apis.controller.js
+++ src/controllers/apis.controller.js
@@ -1,73 +1,79 @@
 import { getConnection, queries, sql } from "../database";
 
 export const getTableData = async (req, res) => {
-    try {
-        const pool = await getConnection();
-        const result = await pool.request().query(queries.getTableData);
-        var obj = {
-            count : 0,
-            entries: result.recordset
-        };
+  try {
+    const pool = await getConnection();
+    const result = await pool.request().query(queries.getTableData);
+    var obj = {
+      count: 0,
+      entries: result.recordset,
+    };
 
-        obj.count = Object.keys(result.recordset).length;
+    obj.count = Object.keys(result.recordset).length;
 
-        res.json(obj)
-    } catch (error) {
-        res.status(500);
-        res.send(error.message);
-    }
+    res.json(obj);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
 };
 
 export const getApiDetail = async (req, res) => {
-    try {
-        const pool = await getConnection();
-        const result = await pool
-        .request()
-        .input("id_api", req.params.id_api)
-        .query(queries.getDetailedAPI);
+  try {
+    const pool = await getConnection();
+    const result = await pool
+      .request()
+      .input("id_api", req.params.id_api)
+      .query(queries.getDetailedAPI);
 
-        res.json(result.recordset[0]);
-
-    } catch (error) {
-        res.status(500);
-        res.send(error.message);
-    }
+    res.json(result.recordset[0]);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
 };
 
 export const createAPI = async (req, res) => {
-    const { nombre_api, version_api, url_base, descripcion_api} = req.body;
-    let { seguridad_api, ult_conexion_api, api_key, id_disp} = req.body;
+  const { nombre_api, version_api, url_base, descripcion_api } = req.body;
+  let { seguridad_api, ult_conexion_api, api_key, disp_api } = req.body;
 
-    // validating
-    if (nombre_api == null || version_api == null || url_base == null || descripcion_api == null) {
-        return res.status(400).json({ msg: "Bad Request. Please fill all fields" });
-    }
+  // validating
+  if (
+    nombre_api == null ||
+    version_api == null ||
+    url_base == null ||
+    descripcion_api == null
+  ) {
+    return res.status(400).json({ msg: "Bad Request. Please fill all fields" });
+  }
 
-    
-    if (seguridad_api == null) seguridad_api = true;
-    if (ult_conexion_api == null) ult_conexion_api = '05-03-22 14:20:09 PM';
-    if (api_key == null) api_key = 'NA';
-    if (id_disp == null) id_disp = 1;
+  var dateTime = require("node-datetime");
+  var dt = dateTime.create();
+  var formatted = dt.format("Y-m-d H:M:S");
 
+  if (seguridad_api == null) seguridad_api = true;
+  if (ult_conexion_api == null) ult_conexion_api = formatted;
+  if (api_key == null) api_key = "NA";
+  if (disp_api == null) disp_api = "13/15";
 
-    try {
-        const pool = await getConnection();
+  try {
+    const pool = await getConnection();
 
-        await pool
-            .request()
-            .input("nombre_api", sql.VarChar, nombre_api)
-            .input("seguridad_api", sql.Bit, seguridad_api)
-            .input("ult_conexion_api", sql.VarChar, ult_conexion_api)
-            .input("version_api", sql.VarChar, version_api)
-            .input("url_base", sql.VarChar, url_base)
-            .input("descripcion_api", sql.VarChar, descripcion_api)
-            .input("api_key", sql.VarChar, api_key)
-            .input("id_disp", sql.Int, id_disp)
-            .query(queries.addNewAPI);
+    await pool
+      .request()
+      .input("nombre_api", sql.VarChar, nombre_api)
+      .input("seguridad_api", sql.Bit, seguridad_api)
+      .input("ult_conexion_api", sql.VarChar, ult_conexion_api)
+      .input("version_api", sql.VarChar, version_api)
+      .input("url_base", sql.VarChar, url_base)
+      .input("descripcion_api", sql.VarChar, descripcion_api)
+      .input("api_key", sql.VarChar, api_key)
+      .input("disp_api", sql.VarChar, disp_api)
+      .query(queries.addNewAPI);
 
-        res.json("Se ha añadido la API.");
-    } catch (error) {
-        res.status(500);
-        res.send(error.message);
-    }
+    res.json("Se ha añadido la API.");
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
 };
--- src/database/queries.js
+++ src/database/queries.js
@@ -3,8 +3,8 @@ export const queries = {
     addNewUser: "IF NOT EXISTS (SELECT email FROM Dbo.Usuario WHERE email = @email)BEGIN INSERT INTO dbo.Usuario (nombre_usr, email, id_tipo_usr) VALUES(@nombre_usr,@email,@id_tipo_usr ); END",
     deleteUser: "DELETE FROM [IPS_API_MANAGEMENT_8].dbo.Usuario WHERE id_usr= @id_usr",
     updateRoleById: "UPDATE  [IPS_API_MANAGEMENT_8].dbo.Usuario SET id_tipo_usr  = @id_tipo_usr  WHERE id_usr = @id_usr",
-    getTableData: "SELECT API.id_api, nombre_api, Disponibilidad.tipo_disp, API.seguridad_api, API.ult_conexion_api, API.version_api FROM API JOIN Disponibilidad ON API.id_disp = Disponibilidad.id_disp",
+    getTableData: "SELECT id_api, nombre_api, disp_api, seguridad_api, ult_conexion_api, version_api from API",
     getUserByEmail: "SELECT Usuario.nombre_usr, Usuario.email, Tipo_Usr.tipo_usr FROM Usuario JOIN Tipo_Usr ON Usuario.id_tipo_usr = Tipo_Usr.id_tipo_usr WHERE email=@email",
     getDetailedAPI: "SELECT *FROM [IPS_API_MANAGEMENT_8].dbo.API WHERE id_api = @id_api",
-    addNewAPI: "INSERT INTO [IPS_API_MANAGEMENT_8].dbo.API (nombre_api, seguridad_api, ult_conexion_api, version_api, url_base, descripcion_api, api_key, id_disp ) VALUES (@nombre_api,@seguridad_api, convert(datetime,@ult_conexion_api,5) ,@version_api,@url_base,@descripcion_api,@api_key,@id_disp );"
+    addNewAPI: "INSERT INTO [IPS_API_MANAGEMENT_8].dbo.API (nombre_api, seguridad_api, ult_conexion_api, version_api, url_base, descripcion_api, api_key, disp_api) VALUES (@nombre_api, @seguridad_api, @ult_conexion_api, @version_api,@url_base,@descripcion_api,@api_key,@disp_api );"
 }
